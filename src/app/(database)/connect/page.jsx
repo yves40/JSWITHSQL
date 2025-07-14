ef5a7c3e7985078c564239116f9352d4970401e0
yves40 <yves.toubhans@free.fr>
2025-07-14 19:08:20 +0200

    Learn mysql with node : connection

--- src/app/(database)/connect/page.jsx
+++ src/app/(database)/connect/page.jsx
@@ -6,21 +6,22 @@ export default async function ConnectDB() {
 
   // const [state, setState ] = useState(0);
 
-  const connectionstatus = await mysqlConnect(); 
-  console.log(connectionstatus);
-  
   let message = '';
-  if(connectionstatus.status === 'connected') {
-    message = "Connection was OK"
-  }
-  else {
-    message = "Connection was not possible, the DB sql server is down"
-  }
-
+  const connectionstatus = await mysqlConnect(); 
+  Promise.all([connectionstatus]).then( (result) => {
+    if(connectionstatus.status) {
+      message = "Connection was OK"
+    }
+    else {
+      message = "Connection was not possible, the DB sql server is down"
+    }
+  })
   return (
     <div className="u-main-container u-padding-content-container">
       <span className=' text-cyan-100'>Connect DB</span>
       <h2 className=' text-white'>{message}</h2>
     </div>
   )
+  
+
 }
--- src/app/lib/server/db.js
+++ src/app/lib/server/db.js
@@ -6,31 +6,33 @@ const dbuser = process.env.DBUSER;
 const dbpass = process.env.DBPASS;
 let connection = null;
 
-export async function mysqlConnect() {
+export function mysqlConnect() {
   let con = mysql.createConnection({
     host: dbhost,
     port: dbport,
     user: dbuser,
     password: dbpass
   });
-  await con.connect(function(err) {
+  // console.log(`************** Connect on ${dbhost}/${dbport}`);
+  return new Promise((resolve, reject) => {
+  con.connect(function(err) {
     if (err) {
-      return { 
-          status: "Disconnected" ,
+      reject({ 
+        error: true,
+          status: "Sorry, an error has been triggered" ,
           message: err
-      };
+      }) 
     }
     else {
-      return { 
+      resolve({ 
+        error: false,
           status: "Connected" ,
           message: "No error"
-      };
+      }) 
     }
   });      
-  // return { 
-  //     status: "KAPUT" ,
-  //     message: "Kaput"
-  // };
+
+  })
 }
 
 export async function mysqlDisconnect() {
