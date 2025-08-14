e94dc2da918ba4d383d09c4c687f1595924e159c
yves40 <yves.toubhans@free.fr>
2025-08-14 15:44:41 +0200

    Promise and SQL select

--- src/sandbox/dbtest.js
+++ src/sandbox/dbtest.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
-
 dotenv.config({ quiet: true });
 
 console.log(`**************** ${process.env.DBHOST}`);
@@ -10,22 +9,54 @@ console.log(`**************** ${process.env.DBUSER}`);
 console.log(`**************** ${process.env.DBPASS}`);
 console.log(`**************** ${process.env.DBNAME}`);
 
-let con = mysql.createConnection({
-  host: process.env.DBHOST,
-  port: process.env.DBPORT,
-  user: process.env.DBUSER,
-  password: process.env.DBPASS,
-  database: process.env.DBNAME
-});
+const dbhost = process.env.DBHOST;
+const dbport = process.env.DBPORT;
+const dbname = process.env.DBNAME;
+const dbuser = process.env.DBUSER;
+const dbpass = process.env.DBPASS;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-con.query("SELECT * FROM users", function (err, result, fields) {
-    if (err) throw err;
-    result.forEach(element => {
-      console.log(`${element.firstname} ${element.lastname} email is ${element.email}`);      
+// ----------------------------------------------------------------------------------------
+let theconnection = null;
+// ----------------------------------------------------------------------------------------
+mysql.createConnection({
+  host: dbhost, 
+  port: dbport,
+  database: dbname,
+  user: dbuser,
+  password: dbpass
+})
+.then(connection => {
+    statusmessage = `Connected now with user ${dbuser} on DB: ${dbname}`;
+    theconnection = connection;
+    console.log(statusmessage);
+    selectSomeUser().then( result => {
+      console.log(result);
+       process.exit(0);
+    })
+    .catch((err) => {
+      console.log(err)
+      process.exit(1)
     });
-    process.exit(0);
-  });
-});
+    
+}) 
+.catch(error => { 
+    statusmessage = 'Connection rejected';
+    dbmessage = error.message;
+    console.log(statusmessage +  ' / ' + dbmessage);
+    process.exit(1);
+})
+// ----------------------------------------------------------------------------------------
+async function selectSomeUser() {
+    try {
+      const [ rows, field ] = await theconnection.query("SELECT * FROM users");
+      rows.forEach(element => {
+        console.log(`${element.firstname} ${element.lastname} email is ${element.email}`);      
+      });
+      Promise.resolve('Done');
+    }
+    catch(err) {
+      console.log(err);
+      Promise.reject('Got a problem here');
+    }
+}
+
