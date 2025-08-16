9453dc41224fecb5c5041b1ab717ffca9ad05c78
yves40 <yves.toubhans@free.fr>
2025-08-16 10:53:16 +0200

    Play with pooling

--- src/sandbox/dbtest.js
+++ src/sandbox/dbtest.js
@@ -15,8 +15,18 @@ const dbname = process.env.DBNAME;
 const dbuser = process.env.DBUSER;
 const dbpass = process.env.DBPASS;
 
+
+
+const promise1 = Promise.resolve(123);
+
+promise1.then((value) => {
+  console.log(value);
+  // Expected output: 123
+});
+
 // ----------------------------------------------------------------------------------------
 let theconnection = null;
+let thepool = null;
 // ----------------------------------------------------------------------------------------
 mysql.createConnection({
   host: dbhost, 
@@ -26,12 +36,25 @@ mysql.createConnection({
   password: dbpass
 })
 .then(connection => {
-    statusmessage = `Connected now with user ${dbuser} on DB: ${dbname}`;
     theconnection = connection;
-    console.log(statusmessage);
+    console.log(`Connected now with user ${dbuser} on DB: ${dbname}`);
     selectSomeUser().then( result => {
       console.log(result);
-       process.exit(0);
+      // Now test a pool
+      getPool().then( result => {
+        selectSomeUserWithPool().then( result  => {
+          console.log(`Everything went fine with the pool`);
+          selectSomeUserWithPoolManual().then(
+            result  => {
+              console.log(`Everything went fine with the manual pool`);
+              process.exit(0);
+            }
+          )
+        });
+      })
+      .catch(err =>  {
+        process.exit(1);
+      })
     })
     .catch((err) => {
       console.log(err)
@@ -50,13 +73,81 @@ async function selectSomeUser() {
     try {
       const [ rows, field ] = await theconnection.query("SELECT * FROM users");
       rows.forEach(element => {
-        console.log(`${element.firstname} ${element.lastname} email is ${element.email}`);      
+        console.log(`DIRECT : ${element.firstname} ${element.lastname} email is ${element.email}`);      
       });
-      Promise.resolve('Done');
+      return Promise.resolve('Everything went fine without any pool');
     }
     catch(err) {
       console.log(err);
-      Promise.reject('Got a problem here');
+      return Promise.reject('Got a problem here');
+    }
+}
+// ----------------------------------------------------------------------------------------
+async function selectSomeUserWithPool() {
+    try {
+      const [ rows, field ] = await thepool.query("SELECT * FROM users");
+      rows.forEach(element => {
+        console.log(`POOL : ${element.firstname} ${element.lastname} email is ${element.email}`);      
+      });
+      return Promise.resolve('Done with the pool');
+    }
+    catch(err) {
+      console.log(err);
+      return Promise.reject('Got a problem here');
+    }
+}
+// ----------------------------------------------------------------------------------------
+async function selectSomeUserWithPoolManual() {
+    try {
+      thepool.getConnection( (err, dynconn) => {
+          dynconn.query("SELECT * FROM users", (err, results, fields) => {
+            dynconn.release();
+            if(err) {
+              console.log(err);
+              return Promise.reject('Got a problem here when manually using a connection from the pool');
+            }
+            else {
+              console.log(`MANUAL : Fine select `);
+              return Promise.resolve('MANUAL : Fine select ');
+            }
+          })
+      });
+
+      const [ rows, field ] = await thepool.query("SELECT * FROM users");
+      rows.forEach(element => {
+        console.log(`POOL : ${element.firstname} ${element.lastname} email is ${element.email}`);      
+      });
+      return Promise.resolve('Done with the pool');
+    }
+    catch(err) {
+      console.log(err);
+      return Promise.reject('Got a problem here');
+    }
+}
+// ----------------------------------------------------------------------------------------
+async function getPool() {
+    try {
+        const pool = mysql.createPool({
+          host: dbhost, 
+          port: dbport,
+          database: dbname,
+          user: dbuser,
+          password: dbpass,
+          waitForConnections: true,
+          connectionLimit: 10,
+          maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+          idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+          queueLimit: 0,
+          enableKeepAlive: true,
+          keepAliveInitialDelay: 0,
+        })
+        thepool = pool;
+        console.log(`Fine, you got a pool !!!`);
+        
+    }
+    catch(err) {
+      console.log(err);
+      Promise.reject('Got a problem here with the pool ');
     }
 }
 
