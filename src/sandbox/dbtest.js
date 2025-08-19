50b92ae43dbf239e549cf777de38fc7fb9cf9bba
yves40 <yves.toubhans@free.fr>
2025-08-19 14:12:20 +0200

    More on Prommises

--- src/sandbox/dbtest.js
+++ src/sandbox/dbtest.js
@@ -1,168 +1,46 @@
-import mysqlPromise from "mysql2/promise.js";
-import dotenv from 'dotenv';
-import  { moduletemplate, sharedidentity }  from './moduletemplate.js';
 import  { moduleSQL}  from './moduleSQL.js';
 
-dotenv.config({ quiet: true });
+console.log(`\nCurrent version of the sql module : ${moduleSQL.getVersion()} \n\n` );
 
-const dbhost = process.env.DBHOST;
-const dbport = process.env.DBPORT;
-const dbname = process.env.DBNAME;
-const dbuser = process.env.DBUSER;
-const dbpass = process.env.DBPASS;
+let action1, action2;
 
-
-// Test module
-
-console.log(moduletemplate.getVersion());
-// console.log(moduletemplate.privateTest()); Inaccessible as not exported by module
-console.log(moduletemplate.Version);
-console.log(sharedidentity);
-console.log(moduleSQL.getVersion());
-
-
-
-// ----------------------------------------------------------------------------------------
-let theconnection = null;
-let thepool = null;
-// ----------------------------------------------------------------------------------------
-mysqlPromise.createConnection({
-  host: dbhost, 
-  port: dbport,
-  database: dbname,
-  user: dbuser,
-  password: dbpass
-})
-.then(connection => {
-    theconnection = connection;
-    console.log(`Connected now with user ${dbuser} on DB: ${dbname}`);
-    // *********
-    selectSomeUser().then( result => {
-      console.log(result);
-      // Now test a pool
-      getPool().then( result => {
-      // *********
-        selectSomeUserWithPool().then( result  => {
-          console.log(`Everything went fine with the pool`);
-        // *********
-        selectSomeUserWithPoolManual().then(
-            result  => {
-              console.log(`Everything went fine with the manual pool`);
-              process.exit(0);
-            }
-          )
-        });
+// Direct connection mode
+moduleSQL.createConnection()
+  .then( message =>  {
+    console.log(message);
+    action1 = moduleSQL.select("SELECT * FROM users")
+      .then( result =>  { 
+        console.log(result);
+        // process.exit(0);
       })
-      .catch(err =>  {
+      .catch( error => {
+        console.log(error);
         process.exit(1);
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-      process.exit(1)
-    });
-    
-}) 
-.catch(error => { 
-    statusmessage = 'Connection rejected';
-    dbmessage = error.message;
-    console.log(statusmessage +  ' / ' + dbmessage);
+      });
+  })
+  .catch( error => {
+    console.log(error);
     process.exit(1);
-})
-// ----------------------------------------------------------------------------------------
-async function selectSomeUser() {
-    try {
-      const [ rows, field ] = await theconnection.query("SELECT * FROM users");
-      rows.forEach(element => {
-        console.log(`DIRECT : ${element.firstname} ${element.lastname} email is ${element.email}`);      
+  });
+// Pooling mode
+moduleSQL.createPool()
+  .then(
+    message =>  {
+    console.log(message);
+    action2 = moduleSQL.poolSelect("SELECT * FROM users order by lastname")
+      .then( result =>  { 
+        console.log(result);
+        // process.exit(0);
+      })
+      .catch( error => {
+        console.log(error);
+        process.exit(1);
       });
-      return Promise.resolve('Everything went fine in direct mode');
-    }
-    catch(err) {
-      console.log(err);
-      return Promise.reject('Got a problem here');
-    }
-}
-// ----------------------------------------------------------------------------------------
-async function selectSomeUserWithPool() {
-    try {
-      const [ rows, field ] = await thepool.query("SELECT * FROM users");
-      rows.forEach(element => {
-        console.log(`POOL : ${element.firstname} ${element.lastname} email is ${element.email}`);      
-      });
-      return Promise.resolve('Done with the pool');
-    }
-    catch(err) {
-      console.log(err);
-      return Promise.reject('Got a problem here');
-    }
-}
-// ----------------------------------------------------------------------------------------
-async function selectSomeUserWithPoolManual() {
-  return new Promise( ( resolve, reject ) => {
-    thepool.getConnection(function(err, conn) {
-      if (err) throw err; // not connected!
-
-      // Use the connection
-      conn.query('SELECT * FROM users', function (error, results) {
-        conn.release();
-        if (error) throw error;
-      });
-    });
-    resolve('********************* ');
+  })
+  .catch(error => {
+    console.log(error);
+    process.exit(1);
   });
 
-    // try {
-    //   thepool.getConnection( function (err, dynconn) {
-    //       if(err) {
-    //         console.log(err);
-    //         return Promise.reject('Got a problem here getConnection() from the pool');
-    //       }
-    //      dynconn.query("SELECT * FROM users", (err, results) => {
-    //         dynconn.releaseConnection();
-    //         if(err) {
-    //           console.log(err);
-    //           return Promise.reject('Got a problem here when manually using a connection from the pool');
-    //         }
-    //         else {
-    //           results.forEach(element => {
-    //             console.log(`MANUAL POOL : ${element.firstname} ${element.lastname} email is ${element.email}`);      
-    //           });
-    //           return Promise.resolve('MANUAL POOL: Fine select ');
-    //         }
-    //       })
-    //   });
-    // }
-    // catch(err) {
-    //   console.log(err);
-    //   return Promise.reject('Got a problem here');
-    // }
-}
-// ----------------------------------------------------------------------------------------
-async function getPool() {
-    try {
-        const pool = mysqlPromise.createPool({
-          host: dbhost, 
-          port: dbport,
-          database: dbname,
-          user: dbuser,
-          password: dbpass,
-          waitForConnections: true,
-          connectionLimit: 10,
-          maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-          idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-          queueLimit: 0,
-          enableKeepAlive: true,
-          charset: 'utf8mb4',
-          keepAliveInitialDelay: 0,
-        })
-        thepool = pool;
-        // console.log(`Fine, you got a pool !!!`);
-        
-    }
-    catch(err) {
-      console.log(err);
-      Promise.reject('Got a problem here with the pool ');
-    }
-}
-
+// Wait for all tsaks to finish
+Promise.all([action1, action2]).then( () => process.exit(0));
--- src/sandbox/moduleSQL.js
+++ src/sandbox/moduleSQL.js
@@ -1,31 +1,116 @@
-/*------------------------------------------------------------------------------------------------------
+/*----------------------------------------------------------------------------------
  *          moduleSQL.js
  *          
  *          Aug 19 2025     Initial
- ------------------------------------------------------------------------------------------------------*/
+ ----------------------------------------------------------------------------------*/
 "use strict";
+
+import mysqlPromise from "mysql2/promise.js";
+import dotenv from 'dotenv';
+
+
+dotenv.config({ quiet: true });
+
+const dbhost = process.env.DBHOST;
+const dbport = process.env.DBPORT;
+const dbname = process.env.DBNAME;
+const dbuser = process.env.DBUSER;
+const dbpass = process.env.DBPASS;
 
 const moduleSQL = (function () {
 
     //---------------- MODULE SCOPE VARIABLES --------------
-    const Version = "moduleSQL.js Aug 19 2025, 1.01";
-    //------------------- EVENT HANDLERS -------------------
-
-    //--------------------- DOM METHODS --------------------
-
+    const Version = "moduleSQL.js Aug 19 2025, 1.03";
+    let connection = null;
+    let pool = null;
     //------------------- PRIVATE METHODS ------------------
-    function privateTest() {
-        return '********** Date from privateTest()';
-    }
 
     //------------------- PUBLIC METHODS -------------------
-
-    function getVersion() {
-        return Version;
+    function getVersion()  { return Version; }
+    // -----------------------------------------------------
+    function createConnection() {
+      return new Promise((resolve, reject) => {
+        mysqlPromise.createConnection({
+          host: dbhost, 
+          port: dbport,
+          database: dbname,
+          user: dbuser,
+          password: dbpass
+        })
+        .then(result => {
+          connection = result;
+          resolve('Connection acquired');
+        })
+        .catch( error => {
+          console.log(`KO error : ${error}`);
+          reject(`Got a problem with connection ${error}`);
+        })
+      })
     }
-
+    // -----------------------------------------------------
+    function createPool() {
+      return new Promise((resolve, reject) => {
+        try {
+          pool = mysqlPromise.createPool({
+             host: dbhost, 
+             port: dbport,
+             database: dbname,
+             user: dbuser,
+             password: dbpass,
+             waitForConnections: true,
+             connectionLimit: 10,
+             maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+             idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+             queueLimit: 0,
+             enableKeepAlive: true,
+             charset: 'utf8mb4',
+             keepAliveInitialDelay: 0,
+           })
+           resolve('Connection pool acquired');
+        }
+        catch( error ) {
+          console.log(`KO error : ${error}`);
+          reject(`Got a problem with connection pooling ${error}`);
+        }
+      })
+    }
+    // -----------------------------------------------------
+    async function select(query) {
+        try {
+          const [ rows, field ] = await connection.query(query);
+          rows.forEach(element => {
+            console.log(`DIRECT : ${element.firstname} ${element.lastname} email is ${element.email}`);      
+          });
+          return Promise.resolve('Everything went fine in direct mode');
+        }
+        catch(err) {
+          console.log(err);
+          return Promise.reject('Got a problem here');
+        }
+    }
+    // -----------------------------------------------------
+    async function poolSelect(query) {
+        try {
+          const [ rows, field ] = await pool.query(query);
+          rows.forEach(element => {
+            console.log(`POOL : ${element.firstname} ${element.lastname} email is ${element.email}`);      
+          });
+          return Promise.resolve('Everything went fine in pooling mode');
+        }
+        catch(err) {
+          console.log(err);
+          return Promise.reject('Got a problem with pooling');
+        }
+    }
+    // -----------------------------------------------------
+    // What is exposed outside module ? 
+    // -----------------------------------------------------
     return {
         getVersion: getVersion,
+        createConnection: createConnection,
+        createPool: createPool,
+        select: select,
+        poolSelect: poolSelect
     };
 }());
 
